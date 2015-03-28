aed5ac8347adbe5f6225ee912cc41295609d8616
Kristján Patrekur Þorsteinsson <krillinn@gmail.com>
2015-03-28 23:07:25 +0000

    for u gunnar

--- app/scripts/titans.js
+++ app/scripts/titans.js
@@ -2,6 +2,11 @@
  * Here we should take care of starting and reseting the tians
  * TODO must not repet my self as much as i am doing here... must fix it.
 */
+
+
+
+//gunnar næsta skref er að setja setTimeout() á þetta svo einn komi í einu
+//eða held það 
 var Titans = {
 	start: function() {
 		$(".Titan1").animate( {left: '-10em'}, 10000, "linear" );
